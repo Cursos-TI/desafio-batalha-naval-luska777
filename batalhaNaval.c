127443080c6051b8791dd61c1be64b8cbb71409f
luska777 <118298294+luska777@users.noreply.github.com>
2025-03-28 02:08:02 +0000

    DEsafio mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
-
 #define LINHAS 10
 #define COLUNAS 10
 
-int main(){
+int main() {
+    int tabuleiro[LINHAS][COLUNAS];
 
-    int tabuleiro [LINHAS][COLUNAS];
-
-    for (int i = 0; i < LINHAS; i++){
-        for (int j = 0; j < COLUNAS; j++){
+    // Inicializar o tabuleiro com água (0)
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
             tabuleiro[i][j] = 0;
+        }
+    }
+
+    // Imprimir o tabuleiro
+    printf("  A B C D E F G H I J \n");
+    for (int i = 0; i < LINHAS; i++) {
+        printf("%d ", i);
+        for (int j = 0; j < COLUNAS; j++) {
+            // Condição para imprimir a cruz
+            if ((i >= 1 && i <= 3 && j == 2) || (j >= 1 && j <= 3 && i == 2)) {
+                printf("1 "); // Imprimir tiro
+            }
+
+
+            // Condição para imprimir o triângulo
+            else if (i == 7 && j == 7) {
+                printf("1 "); // Imprimir tiro
+            } else if (i == 8 && j >= 6 && j <= 8) {
+                printf("1 "); // Imprimir tiro
+            } else if (i == 9 && j >= 5 && j <= 9) {
+                printf("1 "); // Imprimir tiro
+            }
+
+
+            // Condição para imprimir o losango
+            else if (i == 1 && j == 7) {
+                printf("1 "); // Imprimir tiro
+            } else if (i == 2 && j >= 6 && j <= 8) {
+                printf("1 "); // Imprimir tiro
+            } else if (i == 3 && j >= 5 && j <= 9) {
+                printf("1 "); // Imprimir tiro
+            } else if (i == 4 && j >= 6 && j <= 8) {
+                printf("1 "); // Imprimir tiro
+            } else if (i == 5 && j == 7) {
+                printf("1 "); // Imprimir tiro
+            } else {
+                printf("%d ", tabuleiro[i][j]); // Imprimir água
             }
         }
-
-    tabuleiro [3][3] = 3;
-    tabuleiro [3][4] = 3;
-    tabuleiro [3][5] = 3;
-
-    tabuleiro [5][8] = 3;
-    tabuleiro [6][8] = 3;
-    tabuleiro [7][8] = 3;
-
-    tabuleiro [4][2] = 3;
-    tabuleiro [5][3] = 3;
-    tabuleiro [6][4] = 3;
-
-    tabuleiro [5][5] = 3;
-    tabuleiro [4][6] = 3;
-
-    printf("  A B C D E F G H I J \n");
-    for (int i = 0; i < LINHAS; i++){
-        printf("%d ", i);
-         for(int j = 0; j < COLUNAS; j++) {
-             printf("%d ", tabuleiro[i][j]);
-             }
         printf("\n");
+    }
 
-    }   
-    
+    return 0;
 }
